251cd2a46cf11137cf5831e8f3bf5e97ca4eb915
Taha Sökmen <resimli1000@gmail.com>
2023-06-22 20:53:38 +0300

    Optimize images

--- components/client/ProductImageDisplayer.jsx
+++ components/client/ProductImageDisplayer.jsx
@@ -1,22 +1,40 @@
 "use client";
 import { useState } from "react";
 import styles from "./ProductImageDisplayer.module.css";
+import Image from "next/image";
 export default function ProductImageDisplayer({ product }) {
   const [currentImageSrc, setCurrentImageSrc] = useState(
     product.productThumbnailPath
   );
   return (
     <div className={styles.productImageContainer}>
-      <img src={currentImageSrc} className={styles.productShowcaseImage}></img>
+      <Image
+        src={currentImageSrc}
+        className={styles.productShowcaseImage}
+        width={1892}
+        height={1430}
+        style={{
+          width: "100%",
+        }}
+        alt="Product thumbnail"
+      />
       <div className={styles.productImages}>
         {product.productImages.map((i) => (
-          <img
+          <Image
             key={i}
             src={i}
+            width={1892}
+            height={1430}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "contain",
+            }}
             onClick={() => {
               setCurrentImageSrc(i);
             }}
-          ></img>
+            alt="Product images"
+          />
         ))}
       </div>
     </div>
